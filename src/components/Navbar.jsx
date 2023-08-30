b040ecbf447b85160814af5e3a122c9d8b512f9b
Abcodez <abdul.b0227@gmail.com>
2023-08-30 03:15:15 -0400

    Navbar Basics

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,27 @@ import styles from "@/styles/Components/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.container}>
-			<h1>Netflix</h1>
+			<h1>
+        <img src="./images/Logonetflix.png" alt="logo"/>
+      </h1>
 			<ul>
-				<li className={styles.home}>
+				<li>
 					<a href="#">Home</a>
 				</li>
 				<li>
-					<a href="#">About</a>
+					<a href="#">TV Shows</a>
+				</li>
+        <li>
+					<a href="#">Movies</a>
+				</li>
+        <li>
+					<a href="#">Latest</a>
+				</li>
+        <li>
+					<a href="#">My List</a>
+				</li>
+        <li>
+					<button>SEARCH</button>
 				</li>
 			</ul>
 		</nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,33 +3,9 @@ import { useState } from "react";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
-	const [state, setState] = useState(0);
-
-	console.log(state);
-
-	function handleIncrement() {
-		setState(state + 1);
-	}
-
-	function handleDecrement() {
-		setState(state - 1);
-	}
-
 	return (
 		<div>
-			<Navbar />
-			<button onClick={handleIncrement}>+ 1</button>
-			<p>{state}</p>
-			<button onClick={handleDecrement}>- 1</button>
-
-			{state > 10 && (
-				<div>
-					<p>hi</p>
-					<h1>Hello World</h1>
-				</div>
-			)}
-
-			{/* <h1>{state > 10 ? "Hello World" : "Goodbye"}</h1> */}
+			<Navbar/>
 		</div>
 	);
 }
