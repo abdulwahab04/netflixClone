86f7e99c8ea4d87357de93843c6523aa6c774ff6
Abdul Basit <abdul.b0227@gmail.com>
2023-08-30 00:38:47 -0400

    first issue wab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import styles from "@/styles/Components/Navbar.module.css";
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+	return (
+		<nav className={styles.container}>
+			<h1>Netflix</h1>
+			<ul>
+				<li className={styles.home}>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
+	const [state, setState] = useState(0);
 
-	console.log('Hello World');
+	console.log(state);
+
+	function handleIncrement() {
+		setState(state + 1);
+	}
+
+	function handleDecrement() {
+		setState(state - 1);
+	}
 
 	return (
 		<div>
-			<h1 className="text-4xl font-bold text-center">Hello World</h1>
+			<Navbar />
+			<button onClick={handleIncrement}>+ 1</button>
+			<p>{state}</p>
+			<button onClick={handleDecrement}>- 1</button>
+
+			{state > 10 && (
+				<div>
+					<p>hi</p>
+					<h1>Hello World</h1>
+				</div>
+			)}
+
+			{/* <h1>{state > 10 ? "Hello World" : "Goodbye"}</h1> */}
 		</div>
 	);
 }
