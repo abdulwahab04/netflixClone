996fa0230eacf42e7b0d70a3d8e82b5c4ba9876d
Abcodez <wahab0.0a8@gmail.com>
2023-09-01 23:56:11 -0400

    Navbar Search Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,55 @@
 import React from "react";
 import styles from "@/styles/Components/Navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import icons
+import {
+  faMagnifyingGlass,
+  faHighlighter,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Navbar() {
-	console.log('hey');
-	return (
-		<nav className={styles.container}>
-			<h1>
-        <img src="./images/Logonetflix.png" alt="logo"/>
-      </h1>
-			<ul>
-				<li>
-					<a href="#">Home</a>
-				</li>
-				<li>
-					<a href="#">TV Shows</a>
-				</li>
+  const [search, setSearch] = useState(false);
+  const handleSearch = () => {
+    setSearch(!search);
+  };
+
+  return (
+    <nav className={styles.container}>
+      <ul className={styles.links}>
+        <img src="./images/Logonetflix.png" alt="logo" />
         <li>
-					<a href="#">Movies</a>
-				</li>
+          <a href="#">Home</a>
+        </li>
         <li>
-					<a href="#">Latest</a>
-				</li>
+          <a href="#">TV Shows</a>
+        </li>
         <li>
-					<a href="#">My List</a>
-				</li>
+          <a href="#">Movies</a>
+        </li>
         <li>
-					<button>SEARCH</button>
-				</li>
-			</ul>
-		</nav>
-	);
+          <a href="#">Latest</a>
+        </li>
+        <li>
+          <a href="#">My List</a>
+        </li>
+      </ul>
+      <ul className={styles.searchBox}>
+        <li>
+          <button onClick={handleSearch}>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          </button>
+        </li>
+        {search && (
+          <li className={styles.searchBar}>
+            <input type="search" placeholder="Search" />
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
-	return (
-		<div>
-			<Navbar/>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
